7690713c6d2853acb2cd57586fe498509ed44fe9
anshul-create <as9572298@gmail.com>
2026-06-27 02:11:18 +0530

    Added service layer

---
+++ patient-service/src/main/java/com/service/PatientService.java
@@ -0,0 +1,11 @@
+package com.service;
+
+import org.springframework.stereotype.Service;
+
+import com.repository.PatientRepository;
+
+@Service
+public class PatientService {
+    private PatientRepository patientRepository;
+    
+}
